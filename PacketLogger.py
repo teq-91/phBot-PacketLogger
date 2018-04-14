0860775e2fae7890a76a72712ba6c69565f466f4
hyteq <hyteq@users.noreply.github.com>
2018-04-14 11:45:12 +0200

    sourcecode optimization

--- PacketLogger.py
+++ PacketLogger.py
@@ -5,56 +5,63 @@ import binascii
 
 gui = QtBind.init(__name__, 'PacketLogger')
 
-paddingLeft = 260
-paddingTop  = 125
+padding_left = 260
+padding_top = 125
 
-QtBind.createLabel(gui, 'client to server', paddingLeft+11, paddingTop+10)
-QtBind.createButton(gui, 'log_client_start', 'start', paddingLeft+10, paddingTop+30)
-QtBind.createButton(gui, 'log_client_stop', 'stop', paddingLeft+10, paddingTop+60)
+QtBind.createLabel(gui, 'client to server', padding_left + 11, padding_top + 10)
+QtBind.createButton(gui, 'log_client_start', 'start', padding_left + 10, padding_top + 30)
+QtBind.createButton(gui, 'log_client_stop', 'stop', padding_left + 10, padding_top + 60)
 
-QtBind.createLabel(gui, 'server to client', paddingLeft+111, paddingTop+10)
-QtBind.createButton(gui, 'log_server_start', 'start', paddingLeft+110, paddingTop+30)
-QtBind.createButton(gui, 'log_server_stop', 'stop', paddingLeft+110, paddingTop+60)
+QtBind.createLabel(gui, 'server to client', padding_left + 111, padding_top + 10)
+QtBind.createButton(gui, 'log_server_start', 'start', padding_left + 110, padding_top + 30)
+QtBind.createButton(gui, 'log_server_stop', 'stop', padding_left + 110, padding_top + 60)
 
-logClient = False
-logServer = False
+log_client = False
+log_server = False
+
 
 def handle_silkroad(opcode, data):
-	if logClient == True:
-		log('[%s]: packet from client to server' %(__name__))
-		log('[%s]: \topcode: 0x%02X' %(__name__, opcode))
-		if data is not None:
-			log('[%s]: \tdata: %s' %(__name__, binascii.hexlify(data)))
-	return True
+    if logClient:
+        log('[%s]: packet from client to server' % (__name__))
+        log('[%s]: \topcode: 0x%02X' % (__name__, opcode))
+        if data is not None:
+            log('[%s]: \tdata: %s' % (__name__, binascii.hexlify(data)))
+    return True
+
 
 def handle_joymax(opcode, data):
-	if logServer == True:
-		if opcode == 0x2002:
-			return True # we don't want to log pings
-		log('[%s]: packet from server to client' %(__name__))
-		log('[%s]: \topcode: 0x%02X' %(__name__, opcode))
-		if data is not None:
-			log('[%s]: \tdata: %s' %(__name__, binascii.hexlify(data)))
-	return True
+    if logServer:
+        if opcode == 0x2002:
+            return True  # we don't want to log pings
+        log('[%s]: packet from server to client' % (__name__))
+        log('[%s]: \topcode: 0x%02X' % (__name__, opcode))
+        if data is not None:
+            log('[%s]: \tdata: %s' % (__name__, binascii.hexlify(data)))
+    return True
+
 
 def log_client_start():
-	log('[%s]: log client started' %(__name__))
-	global logClient
-	logClient = True
+    global log_client
+    log('[%s]: log client started' % (__name__))
+    log_client = True
+
 
 def log_client_stop():
-	log('[%s]: log client stopped' %(__name__))
-	global logClient
-	logClient = False
+    global log_client
+    log('[%s]: log client stopped' % (__name__))
+    log_client = False
+
 
 def log_server_start():
-	log('[%s]: log server started' %(__name__))
-	global logServer
-	logServer = True
+    global log_server
+    log('[%s]: log server started' % (__name__))
+    log_server = True
+
 
 def log_server_stop():
-	log('[%s]: log server stopped' %(__name__))
-	global logServer
-	logServer = False
+    global log_server
+    log('[%s]: log server stopped' % (__name__))
+    log_server = False
 
-log('[%s] loaded' %(__name__))
+
+log('[%s] loaded' % (__name__))
